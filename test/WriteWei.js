46314ab4b8557ee4025ad1536cef66d177a13556
Chance Hudson <jchancehud@gmail.com>
2019-01-21 19:40:20 -0600

    Add failure cases

--- test/WriteWei.js
+++ test/WriteWei.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const BN = require('bn.js');
 
 contract('WriteWei', accounts => {
-  
+
   it('should create a document', async () => {
     const _contract = await WriteWei.deployed();
     const contract = new web3.eth.Contract(_contract.abi, _contract.address);
@@ -40,6 +40,20 @@ contract('WriteWei', accounts => {
     assert.equal(authorBalance - oldAuthorBalance, paymentValue);
   });
 
+  it('should fail to pay invalid document index', async () => {
+    const _contract = await WriteWei.deployed();
+    const contract = new web3.eth.Contract(_contract.abi, _contract.address);
+    const documentCid = 'QmaafpMEtK4ts455XJ2EyWXxuLzgi2hR8jNUjH2CMToXdn';
+    const author = accounts[0];
+    const paymentValue = 1000;
+    const documentIndex = await contract.methods.documentCount().call() + 100;
+    await assert.rejects(contract.methods.payDocument(documentIndex).send({
+      from: accounts[1],
+      value: paymentValue,
+      gas: 300000
+    }));
+  });
+
   it('should delete a document', async () => {
     const _contract = await WriteWei.deployed();
     const contract = new web3.eth.Contract(_contract.abi, _contract.address);
@@ -63,6 +77,34 @@ contract('WriteWei', accounts => {
     const createdDocument = await contract.methods.documents(documentIndex).call();
     assert.equal(createdDocument.isDeleted, true);
     assert.equal(createdDocument.cid, '');
+  });
+
+  it('should fail to delete from non-author', async () => {
+    const _contract = await WriteWei.deployed();
+    const contract = new web3.eth.Contract(_contract.abi, _contract.address);
+    const documentCid = 'QmaafpMEtK4ts455XJ2EyWXxuLzgi2hR8jNUjH2CMToXdn';
+    const author = accounts[0];
+    await contract.methods.createDocument(documentCid).send({
+      from: author,
+      gas: 300000
+    });
+    const documentIndex = await contract.methods.documentCount().call() - 1;
+    await assert.rejects(contract.methods.deleteDocument(documentIndex).send({
+      from: accounts[1],
+      gas: 300000
+    }));
+  });
+
+  it('should fail to delete invalid document index', async () => {
+    const _contract = await WriteWei.deployed();
+    const contract = new web3.eth.Contract(_contract.abi, _contract.address);
+    const documentCid = 'QmaafpMEtK4ts455XJ2EyWXxuLzgi2hR8jNUjH2CMToXdn';
+    const author = accounts[0];
+    const documentIndex = await contract.methods.documentCount().call() + 100;
+    await assert.rejects(contract.methods.deleteDocument(documentIndex).send({
+      from: author,
+      gas: 300000
+    }));
   });
 
   it('should withdraw author balance', async () => {
