8309539c738a8d4b323d6083901fa9b3cf012437
Chance Hudson <jchancehud@gmail.com>
2019-01-21 19:21:15 -0600

    Fix should create document test

--- test/WriteWei.js
+++ test/WriteWei.js
@@ -6,6 +6,15 @@ contract('WriteWei', accounts => {
   it('should create a document', async () => {
     const _contract = await WriteWei.deployed();
     const contract = new web3.eth.Contract(_contract.abi, _contract.address);
-    await contract.methods.createDocument('test');
+    const documentCid = 'QmaafpMEtK4ts455XJ2EyWXxuLzgi2hR8jNUjH2CMToXdn';
+    const author = accounts[0];
+    await contract.methods.createDocument(documentCid).send({
+      from: author,
+      gas: 300000
+    });
+    const documentIndex = await contract.methods.documentCount().call() - 1;
+    const createdDocument = await contract.methods.documents(documentIndex).call();
+    assert.equal(createdDocument.cid, documentCid);
+    assert.equal(createdDocument.author, author);
   });
 });
